324af40ec0ade46636e37956a7c56fffa96cc62d
Abdul-hadiii <Abdul.hadi@dexterztechnologies.com>
2026-03-17 21:03:00 +0500

    desktop app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as path from 'path';
 
 @Controller('app')
 export class AppController {
   @Get('version')
   getVersion() {
     return {
-      version: '1.0.7',
-      downloadUrl: 'http://3.144.130.126/downloads/HRMS Tracker Setup 1.0.7.exe',
+      version: '1.0.0',
+      downloadUrl: 'http://3.144.130.126/downloads/desktop-app',
       releaseNotes: 'Production build - Connected to production backend',
       mandatory: false,
     };
+  }
+
+  @Get('downloads/desktop-app')
+  downloadDesktopApp(@Res() res: Response) {
+    const filePath = path.join(__dirname, '../../public/downloads/HRMS-Desktop-App-Setup.exe');
+    res.download(filePath, 'HRMS-Desktop-App-Setup.exe');
   }
 }
